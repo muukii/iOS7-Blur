88cac01e86c681c5a765e758cfdcd6306a0028e8
muukii <muukii.muukii@gmail.com>
2013-11-14 19:17:55 +0900

    ios6 apply

--- UIView+blur.m
+++ UIView+blur.m
@@ -5,17 +5,27 @@
 //  Created by Muukii on 9/28/13.
 //  Copyright (c) 2013 Muukii. All rights reserved.
 //
-
 #import "UIView+blur.h"
+#import <QuartzCore/QuartzCore.h>
 #import "UIImage+ImageEffects.h"
+
 @implementation UIView (blur)
 -(UIImage *)blurredSnapshot
 {
-    UIGraphicsBeginImageContextWithOptions(self.bounds.size, NO, self.window.screen.scale);
-    [self drawViewHierarchyInRect:self.frame afterScreenUpdates:NO];
-    UIImage *snapshotImage = UIGraphicsGetImageFromCurrentImageContext();
-    UIImage *blurredSnapshotImage = [snapshotImage applyLightEffect];
-    UIGraphicsEndImageContext();
+    UIImage *blurredSnapshotImage;
+    if ([UIDevice currentDevice].systemVersion.floatValue < 7.0) {
+        UIGraphicsBeginImageContextWithOptions(self.bounds.size, NO, self.window.screen.scale);
+        [self.layer renderInContext:UIGraphicsGetCurrentContext()];
+        UIImage *snapshotImage = UIGraphicsGetImageFromCurrentImageContext();
+        blurredSnapshotImage = [snapshotImage applyLightEffect];
+        UIGraphicsEndImageContext();
+    } else {
+        UIGraphicsBeginImageContextWithOptions(self.bounds.size, NO, self.window.screen.scale);
+        [self drawViewHierarchyInRect:self.frame afterScreenUpdates:NO];
+        UIImage *snapshotImage = UIGraphicsGetImageFromCurrentImageContext();
+        blurredSnapshotImage = [snapshotImage applyLightEffect];
+        UIGraphicsEndImageContext();
+    }
     return blurredSnapshotImage;
 }
 @end
